f63cd32ed0ceae4ddcd8356a55ccf59ea05ec61a
Ivan <palermoivan@hotmail.com>
2024-07-06 13:03:55 -0300

    second page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Box, Button, Card, CardActionArea, CardContent, CardMedia, Grid, Typogr
 //import { MenuList } from '../data/data'
 import { getFirestore, collection, getDocs } from 'firebase/firestore'
 import {app } from '../firebase';
+import Product from './Product';
 
 //importar imagenes
 import Labial1 from '../assets/713651_1_2.jpg';
@@ -15,6 +16,7 @@ import Labial7 from '../assets/576884_1_8.jpg';
 import Labial8 from '../assets/733808_2_8.jpg';
 import Labial9 from '../assets/703728_3_2.jpg';
 import Labial10 from '../assets/703421_1_8.jpg';
+import { Link } from 'react-router-dom';
 
 const images = {
   '713651_1_2.jpg': Labial1,
@@ -33,6 +35,8 @@ const images = {
 export default function Home() {
   
   const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [open, setOpen] = React.useState(false);      // dialog
 
   useEffect(()=>{
     const getProducts = async ()=>{
@@ -49,12 +53,20 @@ export default function Home() {
     }
     getProducts();
   }, []);
+  // dialog
+  const handleClickOpen = (product) => {
+    setSelectedProduct(product);
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedProduct(null);
+  };
 
   return (
     <>
       <Box id="box-home-grid" 
-        sx={{
-          // minHeight: 'calc(100vh - 80px)', 
+        sx={{ 
           marginTop: '80px',
           display: 'flex',
           justifyContent: 'center',
@@ -70,16 +82,22 @@ export default function Home() {
             <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
               <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
                 <CardActionArea>
-                  <CardMedia 
-                    component={'img'} 
-                    sx={{height: 200, objectFit: 'cover'}} 
-                    src={images[product.image]} 
-                    alt={product.name} />
+                  <CardMedia component={'img'} sx={{height: 200, objectFit: 'cover'}} src={images[product.image]} alt={product.name} />
                 </CardActionArea>
-                <CardContent
-                  sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+
+                <CardContent sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                   <Typography variant='h5' gutterBottom component={'div'}>{product.name}</Typography>
-                  <Typography variant='body2' color='text.secondary'>{product.description}</Typography>
+                  {/* <Typography variant='body2' color='text.secondary'>{product.description}</Typography> */}
+
+                  <Typography variant='body1' color="primary"  component={'div'}>Precio : ${product.price}</Typography>
+                  
+                  <Button 
+                    variant="outlined" 
+                    color="warning" 
+                    sx={{ marginTop: 2 }}
+                    onClick={ ()=>handleClickOpen(product)}
+                    >Detalles</Button>
+                  
                   <Button variant="contained" color="primary" sx={{ marginTop: 2 }}>Add to Cart</Button>
                 </CardContent>
                 </Card>
@@ -88,6 +106,7 @@ export default function Home() {
           }
         </Grid>
       </Box>
+      <Product open={open} handleClose={handleClose} product={selectedProduct} images={images}/>
     </>
   )
 }
---
+++ src/pages/Product.jsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { Typography, OutlinedInput, DialogTitle, DialogContentText, DialogContent,  DialogActions, Dialog, Button, Box } from '@mui/material';
+
+
+function Product({ open, handleClose, product, images}) {
+    console.log(product);
+    if(!product){
+      return null;
+    }
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth
+      PaperProps={{
+        sx: {
+          width: '100%',
+          maxWidth: '500px',
+          height: '550px',
+          m: 'auto',
+          borderRadius: 2,
+        }
+      }}
+      >
+      <DialogTitle>
+        <Typography  sx={{ width: '100%', fontSize: 'clamp(1rem, 4vw, 2rem)', color: 'secondary.main' }}>
+          Detalle de Producto
+        </Typography>
+      </DialogTitle>
+
+      <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: 1, }}>
+      <Box component={'img'} 
+          sx={{height: 150, objectFit: 'cover'}}
+          src={images[product.image]}
+          alt={product.name} />
+      
+      <Typography>{product.name}</Typography>
+      <Typography>{product.price}</Typography>
+      <Typography variant='body2'>Descripción:<br/>{product.description}</Typography>
+      <Typography variant='overline' gutterBottom color="primary">Cantidad Disponible: {product.qty}</Typography>
+      <Button variant="contained" color="primary" sx={{ marginTop: 2 }}>Add to Cart</Button>
+      </DialogContent>
+
+      <DialogActions sx={{ pb: 3, px: 3 }}>
+        <Button variant="outlined" onClick={handleClose}>Cancel</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+Product.propTypes = {
+    handleClose: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired,
+  };
+  export default Product;
